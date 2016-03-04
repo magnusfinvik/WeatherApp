2462921b9cd3478b976b5de0df52249b5ee61061
adrja <adrjak.aj@gmail.com>
2016-03-04 13:06:15 +0100

    mewtwo

--- app/src/main/java/com/example/magnusfinvik/weatherapp/MyListFragment.java
+++ app/src/main/java/com/example/magnusfinvik/weatherapp/MyListFragment.java
@@ -48,16 +48,25 @@ public class MyListFragment extends Fragment implements View.OnClickListener, Ad
     }
 
     private void generateGraphView() {
-        graphView = (GraphView)this.getActivity().findViewById(R.id.graph);
-        generateDataFromDB();
-        if(!dataPoints.isEmpty()) {
-            series = new LineGraphSeries<DataPoint>();
-            series.setDrawDataPoints(true);
-            graphView.addSeries(series);
-        }else{
-            Toast toast2 = Toast.makeText(getContext(), "show Data", Toast.LENGTH_SHORT);
-            toast2.show();
-        }
+        //graphView = (GraphView)this.getActivity().findViewById(R.id.graph);
+        //generateDataFromDB();
+        //if(!dataPoints.isEmpty()) {
+        //    series = new LineGraphSeries<DataPoint>();
+        //    series.setDrawDataPoints(true);
+        //    graphView.addSeries(series);
+        //}else{
+        //    Toast toast2 = Toast.makeText(getContext(), "show Data", Toast.LENGTH_SHORT);
+        //    toast2.show();
+       // }
+        GraphView graph = (GraphView) this.getActivity().findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
     }
 
     private void generateDataFromDB() {
